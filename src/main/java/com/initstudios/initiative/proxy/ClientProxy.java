e270453a36922e2b3ca7a45e1b1b715ab7b02f79
Dylan Jones <dljcoolguy@gmail.com>
2018-12-29 10:40:57 +0000

    Added ClientProxy init methods.

--- src/main/java/com/initstudios/initiative/proxy/ClientProxy.java
+++ src/main/java/com/initstudios/initiative/proxy/ClientProxy.java
@@ -3,11 +3,32 @@ package com.initstudios.initiative.proxy;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class ClientProxy extends CommonProxy {
 
 	@Override
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		super.preInit(event);
+	}
+
+	@Override
+	public void init(FMLInitializationEvent event)
+	{
+		super.init(event);
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		super.postInit(event);
+	}
+
+	@Override
 	public void registerItemRenderer(Item item, int meta, String id) {
-		ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(),id));
+		ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), id));
 	}
 }
