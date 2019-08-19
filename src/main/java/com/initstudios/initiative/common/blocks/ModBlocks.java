6f7d9692b2016cf89a097a1367c576b80ce49f21
UKMinecrafted <46700499+UKMinecrafted@users.noreply.github.com>
2019-08-19 09:19:07 -0600

    more disabled un textured blocks

--- src/main/java/com/initstudios/initiative/common/blocks/ModBlocks.java
+++ src/main/java/com/initstudios/initiative/common/blocks/ModBlocks.java
@@ -55,8 +55,8 @@ public class ModBlocks {
     public static final Block PANEL_SURFACE_12 = registerBlock(new NonPortable(Material.IRON), "panel_surface_12", Main.laboratory);
     public static final Block PANEL_SURFACE_13 = registerBlock(new NonPortable(Material.IRON), "panel_surface_13", Main.laboratory);
     public static final Block PANEL_SURFACE_14 = registerBlock(new NonPortable(Material.IRON), "panel_surface_14", Main.laboratory);
-    public static final Block PANEL_SURFACE_21 = registerBlock(new NonPortable(Material.IRON), "panel_surface_21", Main.laboratory);
-    public static final Block PANEL_SURFACE_22 = registerBlock(new NonPortable(Material.IRON), "panel_surface_22", Main.laboratory);
+    //public static final Block PANEL_SURFACE_21 = registerBlock(new NonPortable(Material.IRON), "panel_surface_21", Main.laboratory);
+    //public static final Block PANEL_SURFACE_22 = registerBlock(new NonPortable(Material.IRON), "panel_surface_22", Main.laboratory);
     public static final Block PANEL_SURFACE_31 = registerBlock(new NonPortable(Material.IRON), "panel_surface_31", Main.laboratory);
     public static final Block PANEL_SURFACE_32 = registerBlock(new NonPortable(Material.IRON), "panel_surface_32", Main.laboratory);
     public static final Block PANEL_SURFACE_41 = registerBlock(new LabBlocks(Material.IRON), "panel_surface_41", Main.laboratory);
