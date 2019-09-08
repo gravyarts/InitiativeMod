e55cc31e92d81bc8456210cf9ee50e0bfa30dd57
Dewy <dljcoolguy@gmail.com>
2019-09-08 12:22:58 +0100

    Readable File Size Converter
    + InitiativeIO

---
+++ src/main/java/com/initstudios/initiative/dewy/common/core/utils/InitiativeIO.java
@@ -0,0 +1,20 @@
+package com.initstudios.initiative.dewy.common.core.utils;
+
+import java.text.DecimalFormat;
+
+public class InitiativeIO
+{
+    public static String readableFileSize(long size)
+    {
+        if (size <= 0)
+        {
+            return "0";
+        }
+
+        final String[] units = new String[] { "B", "kB", "MB", "GB", "TB" };
+
+        int digitGroups = (int)(Math.log10(size) / Math.log10(1024));
+
+        return new DecimalFormat("#,##0.#").format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+    }
+}
